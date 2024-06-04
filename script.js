af79f6f9c3046842f3e5324e5b6fece38d0dc32e
Rodrigo Silva <112216918+rodericussilva@users.noreply.github.com>
2024-06-04 17:23:36 -0300

    Update script.js
    Add event.preventDefault()

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
-function validation() {
+function validation(event) {
+  event.preventDefault();
+  
   let fName = document.getElementById("fName");
   let lName = document.getElementById("lName");
   let email = document.getElementById("email");
   let password = document.getElementById("password");
+  let errors = [];
 
   if (!fName.checkValidity()) {
-    document.getElementById("firstnameError").innerHTML =
-      fName.validationMessage;
+    document.getElementById("firstnameError").innerHTML = fName.validationMessage;
+    erros.push('firstnameError');
   }
 
   if (!lName.checkValidity()) {
-    document.getElementById("lastnameError").innerHTML =
-      lName.validationMessage;
+    document.getElementById("lastnameError").innerHTML = lName.validationMessage;
+    errors.push('lastnameError');
   }
 
   if (!email.checkValidity()) {
     document.getElementById("emailError").innerHTML = email.validationMessage;
+    errors.push('emailError');
   }
 
   if (!password.checkValidity()) {
-    document.getElementById("passwordError").innerHTML =
-      password.validationMessage;
+    document.getElementById("passwordError").innerHTML = password.validationMessage;
+    errors.push('passwordError');
   }
 }
