ef8663bce9105dd58cb506ffc72cbcdeb5511bdd
Rodrigo Silva <112216918+rodericussilva@users.noreply.github.com>
2024-06-04 17:39:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -32,4 +32,18 @@ function validation(event) {
       document.getElementById(error).innerHTML = '';
     });
   }, 5000);
+
+  if (errors.length === 0) {
+    alert('Formulário enviado com sucesso!');
+    document.getElementById('registerForm').reset();
+  } else {
+    alert('Por favor, corrija os erros no formulário antes de enviar.');
+  }
+}
+
+  document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById('registerForm');
+    form.addEventListener('submit', validation);
+  });
+
 }
